5a92aa3db2a4943b465c50adf9ec75ff8889bda1
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:38:47 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/apache/karaf/spring/boot/sshd/SshdClientConfig.java
+++ src/main/java/org/apache/karaf/spring/boot/sshd/SshdClientConfig.java
@@ -17,7 +17,7 @@ package org.apache.karaf.spring.boot.sshd;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 
 public class SshdClientConfig {
